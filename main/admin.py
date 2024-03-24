3ad31dabbbd695c48804264e6a1f2a2853947f4b
Tastanbai <138817027+Tastanbai@users.noreply.github.com>
2024-03-25 02:27:38 +0600

    add image

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from main.models import Car, Rental, Feedback
+from main.models import Car, Rental, Feedback, CarImage
 
 # Register your models here.
 admin.site.register(Car)
 admin.site.register(Rental)
 admin.site.register(Feedback)
+admin.site.register(CarImage)
 
 
--- main/models.py
+++ main/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Car(models.Model):
-    image=models.ImageField(upload_to='car_images',null=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     fuel_type = models.CharField(max_length=50)
     volume = models.DecimalField(max_digits=5, decimal_places=2)
@@ -12,6 +11,12 @@ class Car(models.Model):
 def __str__(self):
     return self.name
 
+class CarImage(models.Model):
+    car = models.ForeignKey(Car, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='car_images')
+
+    def __str__(self):
+        return f"Image for car: {self.car.name}"
 class Rental(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     agent = models.CharField(max_length=100)
